9c50d449845a25bf5b40fe7b15629dc0b9a7467d
Gasmyr <thomas@gluu.org>
2018-06-15 15:59:12 +0100

    Fix issue

--- src/test/java/org/gluu/gluuQAAutomation/AllFeatureDryRunTest.java
+++ src/test/java/org/gluu/gluuQAAutomation/AllFeatureDryRunTest.java
@@ -1,14 +1,15 @@
 package org.gluu.gluuQAAutomation;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {
-		"src/main/java/org/gluu/gluuQAAutomation/features" }, monochrome = true, dryRun = true, tags = { "~@gluuQA" })
-//@ActiveProfiles(value = "test")
+		"src/main/java/org/gluu/gluuQAAutomation/features" }, monochrome = true, dryRun = true)
+@ActiveProfiles(value = "test")
 public class AllFeatureDryRunTest {
 
 }
