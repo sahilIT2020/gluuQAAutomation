995126710b97f7ff0041c9e49b89e3af083533a1
Gasmyr <thomas@gluu.org>
2018-07-20 17:03:45 +0100

    AddStrategy

--- src/main/java/org/gluu/gluuQAAutomation/features/AddSocialStrategy.feature
+++ src/main/java/org/gluu/gluuQAAutomation/features/AddSocialStrategy.feature
@@ -3,4 +3,9 @@ Feature: Add social strategy
 Scenario: Add social strategy
 	When 	I sign in as administrator 
 	Then 	I should see gluu home page 
-	When 	I go to strategy page 
+	When 	I go to strategy page
+	And 	I enable passport status to 'Enabled'
+	And 	I save the passport status changed
+	When 	I go to strategy page
+	And 	I add new strategy named 'github' with id 'a97a8b59b74d81887316' and secret 'df71abbb97eef23701af1d1b3402de5a02e883ae'
+	Then 	I sign out 
--- src/main/java/org/gluu/gluuQAAutomation/steps/ManageAuthenticationSteps.java
+++ src/main/java/org/gluu/gluuQAAutomation/steps/ManageAuthenticationSteps.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import cucumber.api.java.After;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 
 @RunWith(SpringRunner.class)
@@ -21,6 +22,21 @@ public class ManageAuthenticationSteps extends BaseSteps {
 	public void goToAuthenticationManagePage() {
 		homePage.goToManageAutheticationMenuPage();
 		authenticationPage.selectTab("Passport Authentication Method");
+	}
+
+	@And("^I add new strategy named '(.+)' with id '(.+)' and secret '(.+)'$")
+	public void addNewStrategy(String name, String id, String secret) {
+		authenticationPage.addStrategy(name, id, secret);
+	}
+
+	@And("^I enable passport status to '(.+)'$")
+	public void setPassportStatus(String status) {
+		authenticationPage.setPassportStatus(status);
+	}
+
+	@And("^I save the passport status changed$")
+	public void save() {
+		authenticationPage.save();
 	}
 
 	@After
