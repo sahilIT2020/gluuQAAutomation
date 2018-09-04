e1cdc29b364a915c406358c39593df14a722dfb9
Gasmyr <thomas@gluu.org>
2018-09-04 23:18:06 +0100

    Disable local test

--- src/test/java/org/gluu/gluuQAAutomation/AllFeatureBeingImplementedTest.java
+++ src/test/java/org/gluu/gluuQAAutomation/AllFeatureBeingImplementedTest.java
@@ -5,9 +5,9 @@ import cucumber.api.junit.Cucumber;
 
 import cucumber.api.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/main/java/org/gluu/gluuQAAutomation/features" }, plugin = { "pretty",
-		"json:target/cucumber/json/cucumber.json" }, monochrome = true, tags = { "@gluuLoadTest" })
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features = { "src/main/java/org/gluu/gluuQAAutomation/features" }, plugin = { "pretty",
+//		"json:target/cucumber/json/cucumber.json" }, monochrome = true, tags = { "@gluuLoadTest" })
 public class AllFeatureBeingImplementedTest {
 
 }
